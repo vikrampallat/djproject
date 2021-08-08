0246bc771382d9b1d640dd12e6541f035e1de430
vikrampallat <76404144+vikrampallat@users.noreply.github.com>
2021-08-08 09:59:14 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+song = "";
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 500, 500, );
+}
+
+function play()
+{
+    song.play();
+}
